83db7d23912428d79b1774510d870c8174a5ff3b
zhihanyue(岳知涵) <zhihanyue@tencent.com>
2018-07-26 16:30:57 +0800

    Fix several critical compatibility issues with Windows

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-require('../index.js')(process.cwd(),{
+require('../index.js')(process.cwd(), {
     d1: ':bar/d1',
     d3: ':foo/d3',
     foo: ':t/foo',
@@ -11,4 +11,3 @@ assert.strictEqual(require(':d1/a'), 1);
 assert.strictEqual(require('~/test/bar/d2/b.js'), 2);
 assert.strictEqual(require('~/test/foo/d3/c.js'), 3);
 assert.strictEqual(require(':d3/c'), 3);
-assert.strictEqual(require('./::t'), 't');
